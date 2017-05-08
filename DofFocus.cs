41e65b67ee705f1ffde135cc9d0e679f6b306aae
bonzaiferroni <lukas8@gmail.com>
2017-05-08 09:21:32 -0600

    Update DofFocus.cs

--- DofFocus.cs
+++ DofFocus.cs
@@ -3,25 +3,20 @@ using UnityEngine.PostProcessing;
 
 public class DofFocus : MonoBehaviour {
     private PostProcessingProfile post;
-    private float defaultValue;
+    private float defaultDistance;
     [SerializeField] private bool mouseMode; // will shoot the ray from where the mouse cursor lies, handy for testing
     [SerializeField] private float focusSpeed = 2f; // change in inspector to adjust rate of focus
 
     private void Start()
     {
         post = GetComponent<PostProcessingBehaviour>().profile;
-        defaultValue = post.depthOfField.settings.focusDistance;
+        defaultDistance = post.depthOfField.settings.focusDistance;
     }
 
     private void Update()
     {
         var target = FindTarget();
         var distance = Vector3.Distance(transform.position, target);
-        ChangeFocus(distance);
-    }
-
-    private void ChangeFocus(float distance)
-    {
         var dof = post.depthOfField.settings;
         dof.focusDistance = Mathf.Lerp(dof.focusDistance, distance, Time.deltaTime * focusSpeed);
         post.depthOfField.settings = dof;
@@ -43,9 +38,10 @@ public class DofFocus : MonoBehaviour {
         return transform.position;
     }
 
-    private void OnDestroy()
-    {
+    private void OnDestroy() {
         // return your focus value to what it was before the scene started
-        ChangeFocus(defaultValue);
+        var dof = post.depthOfField.settings;
+        dof.focusDistance = defaultDistance;
+        post.depthOfField.settings = dof;
     }
 }
